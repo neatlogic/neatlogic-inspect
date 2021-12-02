531c7b202756cec927e07c8fb85b0a2755ed534d
longrf <techsure901>
2021-12-02 13:04:33 +0800

    --story=1005575 --user=龙儒锋 巡检定义 https://www.tapd.cn/54247054/s/1192004

--- src/main/java/codedriver/module/inspect/api/CollectionSearchApi.java
+++ src/main/java/codedriver/module/inspect/api/CollectionSearchApi.java
@@ -2,13 +2,13 @@ package codedriver.module.inspect.api;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.inspect.auth.label.INSPECT_MODIFY;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
-import codedriver.module.inspect.auth.label.INSPECT_MODIFY;
 import codedriver.module.inspect.dao.mapper.InspectMapper;
 import com.alibaba.fastjson.JSONObject;
 import com.mongodb.client.FindIterable;
