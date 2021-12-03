27870f8dd6088c3c2a757779209ebfb8130db2cf
longrf <techsure901>
2021-12-03 18:30:15 +0800

    --story=1005575 --user=龙儒锋 巡检定义 https://www.tapd.cn/54247054/s/1192004

--- src/main/java/codedriver/module/inspect/api/InspectCombopSearchApi.java
+++ src/main/java/codedriver/module/inspect/api/InspectCombopSearchApi.java
@@ -1,12 +1,16 @@
 package codedriver.module.inspect.api;
 
 import codedriver.framework.auth.core.AuthAction;
-import codedriver.framework.cmdb.crossover.ICiTypeListCrossoverService;
+import codedriver.framework.cmdb.crossover.ICiCrossoverMapper;
+import codedriver.framework.cmdb.crossover.IResourceTypeTreeApiCrossoverService;
 import codedriver.framework.cmdb.dto.ci.CiVo;
+import codedriver.framework.cmdb.dto.resourcecenter.ResourceTypeVo;
+import codedriver.framework.cmdb.exception.ci.CiNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.crossover.CrossoverServiceFactory;
 import codedriver.framework.inspect.auth.INSPECT_MODIFY;
 import codedriver.framework.inspect.dao.mapper.InspectMapper;
+import codedriver.framework.inspect.dto.InspectCiCombopVo;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Param;
@@ -14,12 +18,10 @@ import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.nacos.common.utils.CollectionUtils;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Service
 @AuthAction(action = INSPECT_MODIFY.class)
@@ -48,24 +50,54 @@ public class InspectCombopSearchApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
 
-        List<CiVo> inspectCiList =inspectMapper.searchInspectCiCombopList();
+        //获取ciId和combopId的关系
+        List<InspectCiCombopVo> inspectCiList = inspectMapper.searchInspectCiCombopList();
 
-        ICiTypeListCrossoverService ciTypeListCrossoverService = CrossoverServiceFactory.getApi(ICiTypeListCrossoverService.class);
-        List<CiVo> cmdbCiList = ciTypeListCrossoverService.getCiTypeList();
-
-        if (CollectionUtils.isEmpty(inspectCiList)) {
-            return cmdbCiList;
+        //获取cmdb的ciType
+        IResourceTypeTreeApiCrossoverService ciTypeListCrossoverService = CrossoverServiceFactory.getApi(IResourceTypeTreeApiCrossoverService.class);
+        List<ResourceTypeVo> resourceTypeList = ciTypeListCrossoverService.getResourceTypeList();
+        List<CiVo> ciList = new ArrayList<>();
+        List<InspectCiCombopVo> ciCombopVoList = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(resourceTypeList)) {
+            List<CiVo> ciVoList = new ArrayList<>();
+            ICiCrossoverMapper ciCrossoverMapper = CrossoverServiceFactory.getApi(ICiCrossoverMapper.class);
+            for (ResourceTypeVo type : resourceTypeList) {
+                CiVo ciVo = ciCrossoverMapper.getCiByName(type.getName());
+                if (ciVo == null) {
+                    throw new CiNotFoundException(type.getName());
+                }
+                ciVoList.add(ciVo);
+            }
+            ciVoList.sort(Comparator.comparing(CiVo::getLft));
+            for (CiVo ciVo : ciVoList) {
+                List<CiVo> ciListTmp = ciCrossoverMapper.getDownwardCiListByLR(ciVo.getLft(), ciVo.getRht());
+                if (CollectionUtils.isNotEmpty(ciListTmp)) {
+                    ciList.addAll(ciListTmp);
+                }
+            }
+            //将List<CIVo>换成List<InspectCiCombopVo>
+            for (CiVo ciVo : ciList) {
+                ciCombopVoList.add(new InspectCiCombopVo(ciVo));
+            }
         }
-        if (CollectionUtils.isEmpty(cmdbCiList)) {
+
+        //若关系表为空，直接返回
+        if (CollectionUtils.isEmpty(inspectCiList)) {
+            return ciCombopVoList;
+        }
+        //若cmdb的ciType列表为空，直接返回
+        if (CollectionUtils.isEmpty(ciList)) {
             return null;
         }
-        for (CiVo ciVo : cmdbCiList) {
-            List<CiVo> ciTmpList = inspectCiList.stream().filter(o -> ObjectUtils.equals(o.getId(), ciVo.getId())).collect(Collectors.toList());
-            if (CollectionUtils.isEmpty(ciTmpList)) {
+        //把关系表的combopId、combopName set到对应的InspectCiCombopVo里面
+        for (InspectCiCombopVo ciCombopVo : ciCombopVoList) {
+            Optional<InspectCiCombopVo> ciTmp = inspectCiList.stream().filter(o -> Objects.equals(o.getId(), ciCombopVo.getId())).findFirst();
+            if (!ciTmp.isPresent()) {
                 continue;
             }
-            ciVo.setCombopId(ciTmpList.get(0).getCombopId());
+            ciCombopVo.setCombopId(ciTmp.get().getCombopId());
+            ciCombopVo.setCombopName(ciTmp.get().getCombopName());
         }
-        return cmdbCiList;
+        return ciCombopVoList;
     }
 }
