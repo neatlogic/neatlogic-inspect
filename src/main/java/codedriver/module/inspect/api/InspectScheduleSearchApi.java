4059aa0f57b9ae7e0ef489ab9511cdd4ee8589ba
赖文韬 <laiwt@techsure.com.cn>
2021-12-07 11:03:08 +0800

    --story=1005592 --user=赖文韬 定时巡检 https://www.tapd.cn/54247054/s/1192817

--- src/main/java/codedriver/module/inspect/api/InspectScheduleSearchApi.java
+++ src/main/java/codedriver/module/inspect/api/InspectScheduleSearchApi.java
@@ -11,10 +11,7 @@ import codedriver.framework.crossover.CrossoverServiceFactory;
 import codedriver.framework.inspect.auth.INSPECT_BASE;
 import codedriver.framework.inspect.dao.mapper.InspectScheduleMapper;
 import codedriver.framework.inspect.dto.InspectScheduleVo;
-import codedriver.framework.restful.annotation.Description;
-import codedriver.framework.restful.annotation.Input;
-import codedriver.framework.restful.annotation.OperationType;
-import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
@@ -48,6 +45,7 @@ public class InspectScheduleSearchApi extends PrivateApiComponentBase {
     }
 
     @Input({@Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字")})
+    @Output({@Param(name = "Return", explode = InspectScheduleVo[].class)})
     @Description(desc = "巡检定时任务列表")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
