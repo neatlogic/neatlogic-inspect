054d64d3c1e6f31cd79d93521bfeb6afca48dbdd
赖文韬 <laiwt@techsure.com.cn>
2022-05-18 11:19:45 +0800

    --story=1006264 --user=赖文韬 巡检最新问题支持发邮件(将最新问题作为附件) https://www.tapd.cn/54247054/s/1228873

--- src/main/java/codedriver/module/inspect/api/InspectNewProblemReportSendEmailApi.java
+++ src/main/java/codedriver/module/inspect/api/InspectNewProblemReportSendEmailApi.java
@@ -26,7 +26,6 @@ import codedriver.module.inspect.service.InspectReportService;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
