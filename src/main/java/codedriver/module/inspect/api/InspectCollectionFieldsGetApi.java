c07e5c88f07fd34d640cea778218f9f8d7c6bd77
lvzk <lvzk@techsure.com.cn>
2022-07-26 19:28:15 +0800

    补充 接口 权限操作注解

--- src/main/java/codedriver/module/inspect/api/InspectCollectionFieldsGetApi.java
+++ src/main/java/codedriver/module/inspect/api/InspectCollectionFieldsGetApi.java
@@ -1,9 +1,13 @@
 package codedriver.module.inspect.api;
 
+import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.inspect.auth.INSPECT_BASE;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -24,6 +28,8 @@ import java.util.Map;
  * @date 2022/5/24 3:38 下午
  */
 @Service
+@AuthAction(action = INSPECT_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
 public class InspectCollectionFieldsGetApi extends PrivateApiComponentBase {
 
     @Resource
--- src/main/java/codedriver/module/inspect/api/InspectCollectionFieldsSaveApi.java
+++ src/main/java/codedriver/module/inspect/api/InspectCollectionFieldsSaveApi.java
@@ -1,9 +1,13 @@
 package codedriver.module.inspect.api;
 
+import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.inspect.auth.INSPECT_MODIFY;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
 import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
 import org.bson.Document;
@@ -16,6 +20,8 @@ import org.springframework.stereotype.Service;
  * @date 2022/5/24 2:36 下午
  */
 @Service
+@AuthAction(action = INSPECT_MODIFY.class)
+@OperationType(type = OperationTypeEnum.UPDATE)
 public class InspectCollectionFieldsSaveApi extends PrivateApiComponentBase {
 
     @Autowired
