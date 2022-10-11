f129866cb617c220b35225cefae0dd6704699c0e
longrf <a5739119>
2022-10-11 10:04:32 +0800

    --story=1006985 --user=龙儒锋 配置管理-应用清单&巡检-应用巡检的查看权限校验逻辑修改 https://www.tapd.cn/54247054/s/1255820

---
+++ src/main/java/codedriver/module/inspect/api/SearchInspectAppModuleApi.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+package codedriver.module.inspect.api;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.cmdb.crossover.IResourceCenterResourceCrossoverService;
+import codedriver.framework.cmdb.dto.resourcecenter.ResourceSearchVo;
+import codedriver.framework.cmdb.dto.resourcecenter.ResourceVo;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BasePageVo;
+import codedriver.framework.crossover.CrossoverServiceFactory;
+import codedriver.framework.inspect.auth.INSPECT_BASE;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.util.TableResultUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author longrf
+ * @date 2022/10/10 17:01
+ */
+
+@Service
+@AuthAction(action = INSPECT_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class SearchInspectAppModuleApi extends PrivateApiComponentBase {
+    @Override
+    public String getName() {
+        return "查询资源中应用模块列表";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "inspect/appmodule/search";
+    }
+
+    @Input({
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "模糊搜索"),
+            @Param(name = "appSystemIdList", type = ApiParamType.JSONARRAY, desc = "应用系统id列表"),
+            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),
+            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数据条目"),
+            @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true")
+    })
+    @Output({
+            @Param(explode = BasePageVo.class),
+            @Param(name = "tbodyList", explode = ResourceVo[].class, desc = "应用模块列表")
+    })
+    @Description(desc = "查询资源中应用模块列表")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        ResourceSearchVo searchVo = paramObj.toJavaObject(ResourceSearchVo.class);
+        IResourceCenterResourceCrossoverService resourceCrossoverService = CrossoverServiceFactory.getApi(IResourceCenterResourceCrossoverService.class);
+        return TableResultUtil.getResult(resourceCrossoverService.getAppModuleList(searchVo), searchVo);
+    }
+}
