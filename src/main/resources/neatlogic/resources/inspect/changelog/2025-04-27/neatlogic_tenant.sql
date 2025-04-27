79293280c560a9400d3936520e5e8736fe5b4628
1437892690@qq.com <1437892690@qq.com>
2025-04-27 11:14:28 +0800

    [修复] error日志报错3
    关联 #[1408534728638464]error日志报错3 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1408534728638464

--- src/main/resources/neatlogic/resources/inspect/changelog/2025-04-27/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/inspect/changelog/2025-04-27/neatlogic_tenant.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `inspect_alert_everyday`
-    CHANGE `alert_object` `alert_object` TEXT CHARSET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '告警对象';
+    CHANGE `alert_object` `alert_object` VARCHAR (750) CHARSET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '告警对象';
 
