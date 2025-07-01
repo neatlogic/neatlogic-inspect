838ad5c2204f08ace9ce97da9b6ba7914cbd283d
lvzk <897706680@qq.com>
2025-07-01 14:55:37 +0800

    [功能]删除废弃定时作业

--- src/main/resources/neatlogic/resources/inspect/changelog/2025-06-25/neatlogic_tenant.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `inspect_alert_everyday` DROP PRIMARY KEY;
-
-ALTER TABLE `inspect_alert_everyday` CHANGE `alert_object` `alert_object` TEXT NOT NULL COMMENT '告警对象';
-
-ALTER TABLE `inspect_alert_everyday` ADD PRIMARY KEY (`resource_id`, `report_time`, `alert_object` (500));
