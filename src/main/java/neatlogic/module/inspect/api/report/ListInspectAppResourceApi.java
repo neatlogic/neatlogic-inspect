5db6e7590c0cbe5bf3d40d1a04a95fd359b8ee65
lvzk <897706680@qq.com>
2026-04-03 11:01:36 +0800

    [修复]“获取巡检应用报告列表”接口 viewName参数类型异常

--- src/main/java/neatlogic/module/inspect/api/report/ListInspectAppResourceApi.java
+++ src/main/java/neatlogic/module/inspect/api/report/ListInspectAppResourceApi.java
@@ -63,7 +63,7 @@ public class ListInspectAppResourceApi extends PrivateApiComponentBase {
             @Param(name = "appSystemId", type = ApiParamType.LONG, desc = "应用id"),
             @Param(name = "appModuleId", type = ApiParamType.LONG, desc = "应用模块id"),
             @Param(name = "envId", type = ApiParamType.LONG, desc = "环境id,envId=-2时表示无配置环境"),
-            @Param(name = "viewName", type = ApiParamType.LONG, desc = "视图名"),
+            @Param(name = "viewName", type = ApiParamType.STRING, desc = "视图名"),
             @Param(name = "inspectStatusList", type = ApiParamType.JSONARRAY, desc = "巡检状态列表"),
             @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),
             @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数据条目"),
